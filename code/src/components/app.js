228ab359457d308656044eb3311a45dc09b05a75
Caroline Granlund <granlundcaroline@gmail.com>
2018-10-18 21:51:58 +0200

    radio assignment completed

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,11 +1,43 @@
 import React from "react"
 
 class App extends React.Component {
+state={
+  channels: []
+}
+
+  componentWillMount(){
+    fetch('http://api.sr.se/api/v2/channels?format=json&size=100')
+    .then(data => data.json())
+    .then((data) => {
+      console.log(data)
+      this.setState({
+        channels: data.channels
+      })
+    })
+  }
 
   render() {
     return (
-      <div>
-        Find me in src/app.js!
+      <div className="container">
+        <div className="channel-container">
+          {
+            this.state.channels.map(item =>
+              <div className="radio-container" style={{
+                  backgroundColor: '#' + item.color
+                }}>
+                <div className="image-container">
+                  <img className="radio-image" src={item.image} />
+                </div>
+                <div className="radio-content-container">
+                  <span>{item.name}</span>
+                  <audio controls>
+                    <source src={item.liveaudio.url} />
+                  </audio>
+                </div>
+              </div>
+            )
+          }
+        </div>
       </div>
     )
   }
